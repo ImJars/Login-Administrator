2020f6bfdf753dba87b339e14478ef88702cfe82
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2024-11-13 19:00:28 -0600

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 //Importando los modulos de Firebase
 import appFirebase from '../src/credenciales'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 const auth = getAuth(appFirebase)
 
-
+//Importando nuestros componentes
+import Login from './components/Login'
+import Home from './components/Home'
 
 function App() {
+
+  const [usuario, setUsuario] = useState(null)
+
+  onAuthStateChanged(auth, (usuarioFirebase) => {
+    if (usuarioFirebase) {
+      setUsuario(usuarioFirebase)
+    } else {
+      setUsuario(null)
+    }
+  }
+  )
+
   return (
     <>
       <div>
-
+        {usuario ? <Home correoUsuario ={ usuario.email} /> : <Login />}
       </div>
     </>
   )
