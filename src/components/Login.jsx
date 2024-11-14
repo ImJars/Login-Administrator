8937daa97fa9aa6ae624f226eccaf02772ef2be8
Angel Ramirez (Jars) <ra374885@uaeh.edu.mx>
2024-11-13 23:05:30 -0600

    Update stylelogin

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Imagen from '../assets/login-img.webp'
 import ImagenProfile from '../assets/profile.jpg'
 
+import appFirebase from '../credenciales'
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+const auth = getAuth(appFirebase)
+
+
 const Login = () => {
+
+  const [registrando, setRegistrando] = useState(false)
   return (
     <div className='container'>
       <div className="row">
         {/* columna mas pequeña formulario */}
         <div className="col-md-4">
           <div className="padre">
-            <div className="card card-body">
+            <div className="card card-body shadow-lg">
               <img src={ImagenProfile} alt="" className='estilo-profile' />
               <form action="">
                 <input type="text" placeholder='Ingresar Email' className='cajaTexto' />
                 <input type="password" placeholder='Ingresa Contraseña' className='cajaTexto' />
-                <button className='btnForm'>Registrarse</button>
+                <button className='btnForm'>{registrando ? 'Registrate': 'Inicia Sesion'}</button>
               </form>
-              <h4><button>Iniciar Sesion</button></h4>
+              <h4 className='texto'>{registrando ? 'Si ya tienes cuenta  ':'No tienes cuenta  '}<button className='btnswitch' onClick={() => setRegistrando(!registrando)}>{registrando ? 'Inicia Sesion':'Registrate'}</button></h4>
             </div>
           </div>
         </div>
